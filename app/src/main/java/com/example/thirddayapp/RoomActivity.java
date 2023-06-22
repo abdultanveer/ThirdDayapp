03b78f53352812b98fb83f2fe5ba65805b2539c0
Abdul <abdul.tanveer@gmail.com>
2023-06-22 08:16:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/thirddayapp/RoomActivity.java
@@ -0,0 +1,40 @@
+package com.example.thirddayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RoomActivity extends AppCompatActivity {
+    EditText name, email, pincode, city, phoneNumber;
+    Button button;
+    int mPersonId;
+    Intent intent;
+    private AppDatabase mDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+        initViews();
+    }
+
+
+    private void initViews() {
+        name = findViewById(R.id.edit_name);
+        email = findViewById(R.id.edit_email);
+        pincode = findViewById(R.id.edit_pincode);
+        city = findViewById(R.id.edit_city);
+        phoneNumber = findViewById(R.id.edit_number);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveButtonClicked();
+            }
+        });
+    }
+
+}
